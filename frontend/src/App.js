c34d27230f181565083e6fdf05f5d2ff7c319bcd
kimhongzhang323 <kim.hong.zhang323@gmail.com>
2024-12-05 17:40:40 +0800

    Update README.md

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,8 +10,8 @@ function App() {
   }, []);
 
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-2xl font-bold text-blue-600">{message}</h1>
+    <div >
+      <h1 >{message}</h1>
     </div>
   );
 }
